89d4aa445b7c44f8ebae060d584c9488b0fc631d
Eliasvanroosbroeck <r0375721@ucll.be>
2022-12-01 19:25:09 +0100

    ChooseDateAndTimeUISteps updated

--- src/test/java/ui/steps/ChooseDateAndTimeUISteps.java
+++ src/test/java/ui/steps/ChooseDateAndTimeUISteps.java
@@ -5,10 +5,17 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.remote.tracing.opentelemetry.SeleniumSpanExporter;
+
 import org.openqa.selenium.support.PageFactory;
 import ui.pages.BookTablePage;
 import ui.pages.Page;
+import static java.time.temporal.ChronoUnit.DAYS;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 
 import static org.junit.Assert.assertTrue;
 
@@ -31,10 +38,14 @@ public class ChooseDateAndTimeUISteps {
     public void it_is_now(Integer dag, Integer maand, Integer jaar, Integer uur, Integer minuut) {
     }
 
-    @When("Jef makes a reservation for {int}\\/{int}\\/{int} {int}:{int}")
+    @When("Jef makes a reservation for {}\\/{}\\/{} {int}:{int}")
     public void jef_makes_a_reservation_for(Integer dag, Integer maand, Integer jaar, Integer uur, Integer minuut) {
+        LocalDate date2 = LocalDate.of(jaar,maand,dag);
+        long days = DAYS.between(date2, LocalDate.now());
+        //adds the number of days to the examinationDate parameter, the ‘future date’
+        LocalDate date = date2.plusDays(days);
         bookTablePage.open();
-        bookTablePage.fillInDate(String.valueOf(jaar), String.valueOf(maand), String.valueOf(dag));
+        bookTablePage.fillInDate(String.valueOf(date.getYear()), String.valueOf(date.getMonthValue()), String.valueOf(date.getDayOfMonth()));
         bookTablePage.fillInHour(String.valueOf(uur), String.valueOf(minuut));
         bookTablePage.findTable();
         bookTablePage.selectRoom("Grote zaal");
@@ -54,8 +65,10 @@ public class ChooseDateAndTimeUISteps {
 
     @When("Jef makes a reservation for Tuesday {int}:{int}")
     public void jef_makes_a_reservation_for_tuesday(Integer uur, Integer minuut) {
+        LocalDate date2 = LocalDate.now();
+        LocalDate tuesday = date2.with(TemporalAdjusters.next(DayOfWeek.TUESDAY));
         bookTablePage.open();
-        bookTablePage.fillInDate("2022", "12", "6");
+        bookTablePage.fillInDate(String.valueOf(tuesday.getYear()), String.valueOf(tuesday.getMonthValue()), String.valueOf(tuesday.getDayOfMonth()));
         bookTablePage.fillInHour(String.valueOf(uur), String.valueOf(minuut));
         bookTablePage.findTable();
         bookTablePage.selectRoom("Grote zaal");
