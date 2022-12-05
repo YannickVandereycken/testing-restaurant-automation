d5e3ed06b21bfc538b6e0604664e04abe034bc16
Alexander-Leemans <alex.leemans@student.ucll.be>
2022-12-05 20:14:26 +0100

    ChooseRoom feature

--- src/test/java/ui/steps/ChooseRoomUISteps.java
+++ src/test/java/ui/steps/ChooseRoomUISteps.java
@@ -4,7 +4,6 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Before;
-import org.junit.Assert.*;
 import org.openqa.selenium.support.PageFactory;
 import ui.pages.ChooseRoomePage;
 import ui.pages.Page;
