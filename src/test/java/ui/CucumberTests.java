d7a5e682f3e3425dcb50d632233d4efa9e204103
Eliasvanroosbroeck <r0375721@ucll.be>
2022-12-07 16:54:00 +0100

    Choose Table finished

--- src/test/java/ui/CucumberTests.java
+++ src/test/java/ui/CucumberTests.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/features/add_to_cart.feature"},
+        features = {"src/test/resources/features/choose_room.feature"},
         tags = ("@UI"),
         glue = {"ui/steps"},
         plugin = {"json:target/ui-test.json"}
